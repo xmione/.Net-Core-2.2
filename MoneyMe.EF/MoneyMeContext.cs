982bfc839cdbd06a84b0eea1e4e5e436be2da45a
Solomio Sisante <solomiosisante@gmail.com>
2020-09-07 21:26:56 +0800

    Fixed MoneyMeContext.OnConfiguring() method to implement Server=(local) and Database=MoneyMe

--- MoneyMe.EF/MoneyMeContext.cs
+++ MoneyMe.EF/MoneyMeContext.cs
@@ -13,7 +13,8 @@ namespace MoneyMe.EF
             //Server=(localdb)\\mssqllocaldb;Database=ContosoUniversity2;Trusted_Connection=True;MultipleActiveResultSets=true
             //optionsBuilder.UseSqlServer(@"Server=(LocalDB)\MSSQLLocalDB;Database=MoneyMe;Trusted_Connection=True;MultipleActiveResultSets=True;");
             //AppDomain.CurrentDomain.SetData("DataDirectory", System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "AppData"));
-            optionsBuilder.UseSqlServer(@"Server=(LocalDB)\MSSQLLocalDB;AttachDBFilename=C:\Projects\MoneyMe\MoneyMe.EF\AppData\MoneyMe.mdf;Database=MoneyMe;Trusted_Connection=Yes;");
+            //optionsBuilder.UseSqlServer(@"Server=(LocalDB)\MSSQLLocalDB;AttachDBFilename=C:\Projects\MoneyMe\MoneyMe.EF\AppData\MoneyMe.mdf;Database=MoneyMe;Trusted_Connection=Yes;");
+            optionsBuilder.UseSqlServer(@"Server=(local);Database=MoneyMe;Trusted_Connection=Yes;");
         }
     }
 }
